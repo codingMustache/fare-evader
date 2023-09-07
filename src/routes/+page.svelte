2a4d6332c1a8f3776af397942b4867e46b0ee1d9
codingMustache <jorge@codingmustache.dev>
2023-09-07 11:59:50 -0500

    locking orientation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,46 @@
 <script>
-  let pwd
-  const submitHandler = () => {if(pwd === 'fuckRTA'){window.location.href = '/ticket'}}
+	let pwd
+	const submitHandler = () => {
+		if (pwd === 'test') {
+			window.location.href = '/ticket'
+		}
+	}
 </script>
 
 <div id="home">
-  <input
-    type="password"
-    bind:value={pwd}
-  />
-  <button on:click={submitHandler}>Submit</button>
+	<input
+		type="password"
+		bind:value={pwd}
+	/>
+	<button on:click={submitHandler}>Submit</button>
 </div>
 
 <style>
-  #home {
-    width: 50%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    margin: auto;
-    padding: 50% 0;
-  }
+	#home {
+		width: 50%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		margin: auto;
+		padding: 50% 0;
+	}
 
-  input {
-    height: 50px;
-    border-radius: 10px;
-    border: 1px solid black;
-    padding: 5px;
-    font-size: larger;
-  }
+	input {
+		height: 50px;
+		border-radius: 10px;
+		border: 1px solid black;
+		padding: 5px;
+		font-size: larger;
+	}
 
-  button {
-    background-color: rgb(104, 104, 254);
-    color: white;
-    height: 50px;
-    margin: 20px;
-    border-radius: 50px;
-    border-style: none;
-    box-shadow: 0 0 10px black;
-    font-size: larger;
-  }
+	button {
+		background-color: rgb(104, 104, 254);
+		color: white;
+		height: 50px;
+		margin: 20px;
+		border-radius: 50px;
+		border-style: none;
+		box-shadow: 0 0 10px black;
+		font-size: larger;
+	}
 </style>
